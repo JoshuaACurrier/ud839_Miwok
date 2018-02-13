e27679073a0e51a2f0e3f7bdc2fc0b91e78db8c0
Josh Currier <jtiken@gmail.com>
2018-02-12 21:37:00 -0800

    Create new list_item.xml for the arrayAdapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,11 +18,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
+import android.widget.GridView;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import java.io.LineNumberReader;
+
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
